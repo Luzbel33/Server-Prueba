5d4dcbb47d24caf713b54a0aacb134ecf2ef33aa
Gianluca <118855670+Luzbel33@users.noreply.github.com>
2023-03-02 21:08:45 -0300

    Nuevo commit/Creando CRUD

--- src/controllers/UsersController.js
+++ src/controllers/UsersController.js
@@ -6,7 +6,7 @@ const usersFilePath = path.join(__dirname, '../data/Users.json');
 const getUsers = () => {
     const users = fs.readFileSync(usersFilePath, 'utf8');
     const usersJson = JSON.parse(users);
-    return usersJson.user;
+    return usersJson.users;
 };
 
 const usersView = (req, res) => {
@@ -26,9 +26,19 @@ const addUserView = (req, res) => {
 
 const addUser = (req, res) => { 
     console.log(req.body)
-    
+    const users = getUsers();
+    const maxID = users.length + 1
+    const newUser = {
+        id: maxID,
+        nombre: req.body.nombre,
+        apellido: req.body.apellido,
+        email: req.body.email
+    }
+    users.push(newUser);
+    const userJson = JSON.stringify({ users: users}, null, 2);
+    fs.writeFileSync(path.join(__dirname,'../data/Users.json'), userJson);
     res.render(path.join(__dirname, '../views/users/addUser.ejs'));
-}
+};
 
 module.exports = {
    usersView,
